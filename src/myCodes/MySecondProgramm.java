806ec06b93e2fb66180daed055e67bf38102f18c
Ashishsawarkar98 <ashishsawarkar95@gmail.com>
2024-01-06 21:27:44 +0530

    I am update my code

--- src/myCodes/MySecondProgramm.java
+++ src/myCodes/MySecondProgramm.java
@@ -4,7 +4,7 @@ public class MySecondProgramm {
 
 	public static void main(String[] args) {
 
-		System.out.println("Jai shree ram");
+		System.out.println("I am a Software Test Engineer");
 	}
 
 }
